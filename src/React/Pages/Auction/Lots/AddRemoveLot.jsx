5d0111881287229cdb785e44e6b09702f35dc5fc
Danielle N <daniellen@Danielles-MacBook-Pro-2.local>
2020-10-19 20:33:43 -0700

    show hide and remove lot buttons and added lightbox

---
+++ src/React/Pages/Auction/Lots/AddRemoveLot.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+
+/* Scripts ---------------------------*/
+import { addLotToBids, removeLotToBids } from 'Redux/bidManager/actions.js';
+
+/* Components ---------------------------*/
+import Button from 'React/Shared/Controls/Button.jsx';
+
+const AddRemoveLot = ({ lot }) => {
+    // Grab Redux Dispatcher to update store
+    const dispatch = useDispatch();
+
+    // Grab Redux Store
+    const { bidManager: { bids } } = useSelector((state) => state);
+
+    // Check if this lot is in our bids
+    const isInBids = bids.find((bid) => {
+        return bid.lot.id === lot.id;
+    });
+
+/*---------------------------
+| Methods
+---------------------------*/
+    const handleOnClick = () => {
+        console.log('You click me. And I should add a lot to bids'); 
+        dispatch(addLotToBids(lot));
+    }
+
+    const handleRemoveLot = () => {
+        console.log('Remove lot'); 
+        dispatch(removeLotToBids(lot));
+    }
+
+    return (
+        <AddRemoveLotStyled className='AddRemoveLot'>
+        {
+            (isInBids)
+            ?  <Button onClick={ handleRemoveLot }>Remove Lot</Button> 
+            :  <Button onClick={ handleOnClick }>Add Lot</Button>
+        }
+        </AddRemoveLotStyled>
+    );
+}
+
+export default AddRemoveLot;
+
+const AddRemoveLotStyled = styled.div`
+    
+`;
---
+++ src/React/Shared/Lightbox.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import keycode from 'keycode';
+
+/* Components ---------------------------*/
+import Button from 'React/Shared/Controls/Button.jsx';
+
+const Lightbox = ({
+    children,
+    isOpen,
+    onClose,
+    headerText
+}) => {
+
+    // When the component mounts (comes into existence)
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            switch(keycode(e)) {
+                case 'esc':
+                    e.stopPropagation();
+                    handleOnClose();
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        // dismount (not in existence)
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+
+    }, []);
+
+    const handleOnClose = () => {
+        onClose();
+    }
+
+    const stopBubbling = (e) => {
+        e.stopPropagation();
+    }
+
+    if (!isOpen) {return '';}
+
+    return (
+        <LightboxStyled className='Lightbox' onClick={ handleOnClose }>
+            <div className="theLight" onClick={ stopBubbling }>
+                <header>
+                    <h2>{headerText}</h2> 
+                </header>
+                <Button className='closeLightbox' onClick={ handleOnClose }>
+                    X
+                </Button>
+                <div className="content">
+                    { children }
+                </div>
+            </div> 
+        </LightboxStyled>
+    );
+}
+
+export default Lightbox;
+
+const LightboxStyled = styled.div`
+    outline: solid 1px red;
+
+    position: fixed;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+
+    background-color: rgba(0,0,0,.75);
+
+    .theLight {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+
+        width: 300px;
+
+        header{
+            background-color: teal;
+            color: white;
+            padding: 10px;
+
+            h2 {
+                margin: 0px;
+                font-size: 20px;
+            }
+        }
+
+        .closeLightbox {
+            position: absolute;
+            right: 0px; top: 0px;
+            margin: 0px;
+        }
+
+        .content {
+            padding: 10px;
+            min-height: 100px;
+        }
+    }
+`;
